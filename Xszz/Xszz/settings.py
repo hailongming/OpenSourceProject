e9bf7dddb7139eaede2148e460d8aa3b569a9a82
hailongming <hailongming@gmail.com>
2018-05-09 15:25:18 +0800

    Update settings.py
    删除了数据库密码

--- Xszz/Xszz/settings.py
+++ Xszz/Xszz/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'SSDUT',
         'USER': 'root',
-        'PASSWORD': 'mhl19970220',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
